c7bc8bb1b88a21dec7726299dc3d4fb7c3893a0d
viniciussluiz <vinicius@sga.pucminas.br>
2024-06-22 17:08:16 -0300

    Commit sem a chave

--- codigo-fonte/backend/app/settings.py
+++ codigo-fonte/backend/app/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1e=jgdeg*%2jmo)6(@8so#m&76r#mbv^5_f+w2@-mackq$xy-o'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
