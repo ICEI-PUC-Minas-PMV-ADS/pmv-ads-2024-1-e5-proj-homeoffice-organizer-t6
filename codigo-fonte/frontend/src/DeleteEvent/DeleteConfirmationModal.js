ec976da26e2416a61d0c9c5348e17d55051cbb34
Alisson <100442612+alessaocarvalho@users.noreply.github.com>
2024-06-25 13:01:00 -0300

    update

--- codigo-fonte/frontend/src/DeleteEvent/DeleteConfirmationModal.js
+++ codigo-fonte/frontend/src/DeleteEvent/DeleteConfirmationModal.js
@@ -1,13 +1,32 @@
 import React from 'react';
+import axiosInstance from '../axiosInstance';
+import { toast } from 'react-toastify';
 
-const DeleteConfirmationModal = ({ collaboratorId, collaboratorName, onDelete, onClose }) => {
+const DeleteConfirmationModal = ({ collaboratorId, collaboratorName, onDelete, onClose, setHomeOfficeEvents, homeOfficeEvents }) => {
+
+    const handleDelete = async () => {
+        try {
+            await axiosInstance.delete(`/collaborator/api/collaborator-date/${collaboratorId}`);
+            
+            const updatedEvents = homeOfficeEvents.filter(event => event.collaboratorId !== collaboratorId);
+            setHomeOfficeEvents(updatedEvents);
+    
+            toast.success('Home office removido com sucesso!');
+        } catch (error) {
+            console.error('Erro ao remover home office:', error);
+            toast.error('Erro ao remover home office.');
+        } finally {
+            onClose();
+        }
+    };
+
     return (
         <div className="modal">
             <div className="modal-content">
                 <h2>Apagar?</h2>
                 <p>Deseja realmente apagar o home office de {collaboratorName}?</p>
                 <div className="modal-actions">
-                    <button onClick={() => onDelete(collaboratorId)}>Sim</button>
+                    <button onClick={handleDelete}>Sim</button>
                     <button onClick={onClose}>Não</button>
                 </div>
             </div>
